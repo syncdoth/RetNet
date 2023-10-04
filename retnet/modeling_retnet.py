fe892cadb95030729cfc9f4c3699beaa1a78f2ce
Sehyun Choi <choisehyun98@gmail.com>
2023-10-04 20:00:45 +0800

    add RetNetForSequenceClassification

--- retnet/modeling_retnet.py
+++ retnet/modeling_retnet.py
@@ -8,8 +8,9 @@ import torch.nn.functional as F
 import torch.utils.checkpoint
 from timm.models.layers import drop_path
 from torch import nn
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 from transformers import top_k_top_p_filtering
-from transformers.modeling_outputs import ModelOutput
+from transformers.modeling_outputs import ModelOutput, SequenceClassifierOutputWithPast
 from transformers.modeling_utils import PreTrainedModel
 from transformers.utils import logging
 
@@ -1121,3 +1122,119 @@ class RetNetForCausalLM(RetNetPreTrainedModel):
             if early_stopping and (token == eos_token_id).all():
                 break
         return generated
+
+
+class RetNetForSequenceClassification(RetNetPreTrainedModel):
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.model = RetNetModel(config)
+        self.score = nn.Linear(config.decoder_embed_dim, self.num_labels, bias=False)
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def get_input_embeddings(self):
+        return self.model.embed_tokens
+
+    def set_input_embeddings(self, value):
+        self.model.embed_tokens = value
+
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        retention_mask: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_retentions: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        forward_impl: Optional[str] = None,
+        recurrent_chunk_size: Optional[int] = None,
+    ) -> Union[Tuple, SequenceClassifierOutputWithPast]:
+
+        if output_retentions is None and output_attentions is not None:
+            output_retentions = output_attentions
+        output_retentions = output_retentions if output_retentions is not None else self.config.output_retentions
+        output_hidden_states = (output_hidden_states if output_hidden_states is not None else
+                                self.config.output_hidden_states)
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        forward_impl = forward_impl if forward_impl is not None else self.config.forward_impl
+        recurrent_chunk_size = recurrent_chunk_size if recurrent_chunk_size is not None else self.config.recurrent_chunk_size
+
+        if retention_mask is None and attention_mask is not None:
+            retention_mask = attention_mask
+
+        outputs = self.model(input_ids,
+                             retention_mask=retention_mask,
+                             past_key_values=past_key_values,
+                             inputs_embeds=inputs_embeds,
+                             output_retentions=output_retentions,
+                             output_hidden_states=output_hidden_states,
+                             return_dict=return_dict,
+                             forward_impl=forward_impl,
+                             use_cache=use_cache,
+                             recurrent_chunk_size=recurrent_chunk_size)
+
+        hidden_states = outputs[0]
+        logits = self.score(hidden_states)
+
+        if input_ids is not None:
+            batch_size = input_ids.shape[0]
+        else:
+            batch_size = inputs_embeds.shape[0]
+
+        if self.config.pad_token_id is None and batch_size != 1:
+            raise ValueError("Cannot handle batch sizes > 1 if no padding token is defined.")
+        if self.config.pad_token_id is None:
+            sequence_lengths = -1
+        else:
+            if input_ids is not None:
+                sequence_lengths = (
+                    torch.eq(input_ids, self.config.pad_token_id).long().argmax(-1) - 1).to(
+                        logits.device)
+            else:
+                sequence_lengths = -1
+
+        pooled_logits = logits[torch.arange(batch_size, device=logits.device), sequence_lengths]
+
+        loss = None
+        if labels is not None:
+            labels = labels.to(logits.device)
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or
+                                              labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(pooled_logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(pooled_logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(pooled_logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(pooled_logits, labels)
+        if not return_dict:
+            output = (pooled_logits,) + outputs[1:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutputWithPast(
+            loss=loss,
+            logits=pooled_logits,
+            past_key_values=outputs.past_key_values,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
